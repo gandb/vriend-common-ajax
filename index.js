e6a31075ce6811074d87d950970096265b1749c4
Edson Carli <gandbranco@gmail.com>
2019-03-23 10:03:45 -0300

    First version

---
+++ index.js
@@ -0,0 +1,26 @@
+ 
+
+ export class Ajax<T>{ 
+
+	public get(url):Promise<T>
+	{
+		return new Promise<T>(function(resolve, reject) {
+    
+	    var xhr = new XMLHttpRequest();
+	    xhr.open('GET', url);
+
+	    xhr.onload = function() { 
+	      if (xhr.status == 200) { 
+	        resolve(JSON.parse(xhr.response) as T);
+	      } else { 
+	        reject(Error(xhr.statusText));
+	      }
+	    }; 
+	    xhr.onerror = function() {
+	      reject(Error("Network Error"));
+	    }; 
+	    xhr.send();
+	  });
+	} 
+}
+ 
