a097bc7440de41d247069108e2c9bc4d48a81ffa
Edson Carli <gandbranco@gmail.com>
2019-03-27 11:50:40 -0300

    Versão final do teste

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -13,50 +13,22 @@ interface Teste
     url:string;
 }
 
-
-let sum=(a:number,b:number)=>a+b;
-
-
-describe('testes genericos', function() {
-  it('sum', function() {
-    let result = sum(5, 2);
-    assert.equal(7,result);
-  }); 
-});
+ 
 
 const urlTest : string = 'https://httpbin.org/get';
 const ajax:Ajax<Teste> = new Ajax<Teste>(); 
 
 describe('Ajax', function() {
-	 
-    
 
-  it('get common use', function(done) { 
-
-	  	this.timeout(10000);
-		ajax.get(urlTest).then(function(response:Teste) {
-			 
-			 assert.equal("https://httpbin.org/get",response.url);
+  it('get common use', function(done) {  
+		ajax.get(urlTest).then(function(response:Teste) { 
+			 assert.equal(urlTest,response.url);
 			 done();
 		}, function(error) {
 			 assert.fail(JSON.stringify(error));
 			done();
-		}); 
-		
-
-		
+		}); 		
 	});
 
 });
- 
-/*
- const ajax:Ajax<Teste> = new Ajax<Teste>();
-        ajax.get(urlTest).then(function(response:Teste) {
-          console.log('Vanilla Javascript REST success!' );
-          console.log(response);
-        }, function(error) {
-          console.error('Vanilla Javascript REST failed!', error);
-
-        });
-        */
-
+  
